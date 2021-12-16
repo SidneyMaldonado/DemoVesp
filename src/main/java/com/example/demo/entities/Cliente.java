044e89970d3aa27dd4d15e621157fe70f80055b0
SidneyMaldonado <sidneymaldonado@gmail.com>
2021-12-16 13:46:47 -0400

    Primeiro Commit

---
+++ src/main/java/com/example/demo/entities/Cliente.java
@@ -0,0 +1,41 @@
+package com.example.demo.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+
+
+    @Id
+    @Column(name="id")
+    private String id;
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="situacao")
+    private Boolean situacao;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+}
